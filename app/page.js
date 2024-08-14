aa90b812c818dc51628774a6ae0653ef158d3a69
Byren Cheema <bscheema@uci.edu>
2024-08-14 13:07:49 -0700

    Import fixes

--- app/page.js
+++ app/page.js
@@ -2,9 +2,8 @@ import Image from "next/image";
 import getStripe from "@/util/get-stripe";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Container } from "@mui/material";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 import Head from "next/head";
-
-
 
 export default function Home() {
   return (
@@ -16,13 +15,18 @@ export default function Home() {
 
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6">Flashcard Creator</Typography>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Flashcard Creator
+          </Typography>
+          <SignedOut>
+            <Button color="inherit">Login</Button>
+            <Button color="inherit">Sign Up</Button>
+          </SignedOut>
           <SignedIn>
             <UserButton />
           </SignedIn>
         </Toolbar>
       </AppBar>
-      
     </Container>
-    );
+  );
 }
